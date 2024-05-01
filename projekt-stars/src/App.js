a97121e4c5b4df3b30a1192631f200bcce2eff35
08Newby15 <130486834+08Newby15@users.noreply.github.com>
2024-05-01 19:52:23 +0200

    Clean up

--- projekt-stars/src/App.js
+++ projekt-stars/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './login/login';
-import { Routes, Route } from "react-router-dom"; // Import only necessary components
+import { Routes, Route } from "react-router-dom"; 
 import Board from './dashboard/Board';
 
 function App() {
   return (
-    <div className="App">  {/* Wrap your application content here (optional) */}
+    <div className="App">  
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Login route for root path */}
-        <Route path="/dashboard/Board" element={<Board />} /> {/* Route for /test path */}
+        <Route path="/" element={<Login />} /> 
+        <Route path="/dashboard/Board" element={<Board />} /> 
       </Routes>
     </div>
   );
